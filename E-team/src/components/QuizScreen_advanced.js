5b7784a89505d3f4749d6910e6536bc1821eec8c
EJ-pro <hsshss2522@naver.com>
2024-11-29 04:17:30 +0900

    Remove old src and public folders

--- E-team/src/components/QuizScreen_advanced.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import QuizScreen from "./QuizScreen"; // 공통 컴포넌트 사용
-
-const QuizScreen_advanced = () => {
-  return <QuizScreen level="advanced" />;
-};
-
-export default QuizScreen_advanced;
