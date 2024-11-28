0f5cb93052cbf5f1b9b6b71c5e202d0ca09446a2
EJ-pro <hsshss2522@naver.com>
2024-11-29 04:21:06 +0900

    test

--- src/components/QuizScreen_intermediate.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import QuizScreen from "./QuizScreen"; // 공통 컴포넌트 사용
-
-const QuizScreen_intermediate = () => {
-  return <QuizScreen level="intermediate" />;
-};
-
-export default QuizScreen_intermediate;
